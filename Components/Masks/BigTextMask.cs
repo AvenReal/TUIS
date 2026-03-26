aca1613eedd9390e63bdc2f2f29ec594be2dfd52
AvenReal <evan.rieber@gmail.com>
2026-03-26 14:49:40 +0900

    Fixed BigFont issue

--- Components/Masks/BigTextMask.cs
+++ Components/Masks/BigTextMask.cs
@@ -1,8 +1,14 @@
 namespace TUIS.Components.Masks;
 
-public class BigTextTextMask : TextMask
+public class BigTextMask : TextMask
 {
-    private static Dictionary<char, char[]> _font = new()
+    public enum FontType
+    {
+        ThreeByThreeClassic,
+    }
+
+
+    private static readonly Dictionary<char, char[]> ThreeByThreeClassic = new()
     {
         { 'A', new[] { '▄', '▀', '▄', '█', '▄', '█', '█', ' ', '█' } },
         { 'a', new[] { '▄', '▄', ' ', ' ', '▄', '█', '▀', '▄', '█' } },
@@ -17,7 +23,7 @@ public class BigTextTextMask : TextMask
         { 'd', new[] { ' ', ' ', '█', '▄', '▀', '█', '▀', '▄', '█' } },
 
         { 'E', new[] { '█', '▀', '▀', '█', '▀', '▀', '█', '▄', '▄' } },
-        { 'e', new[] { ' ', '▄', ' ', '█', '▄', '█', '▀', '▄', '▄' } },
+        { 'e', new[] { ' ', ' ', ' ', '▄', '▀', '▄', '▀', '█', '▄' } },
 
         { 'F', new[] { '█', '▀', '▀', '█', '▀', ' ', '█', ' ', ' ' } },
         { 'f', new[] { ' ', '▄', '▀', '▀', '█', '▀', ' ', '█', ' ' } },
@@ -61,29 +67,46 @@ public class BigTextTextMask : TextMask
         { 'S', new[] { '▄', '▀', '▀', ' ', '▀', '▄', '▄', '▄', '▀' } },
         { 's', new[] { ' ', '▄', '▄', '▀', '▄', ' ', '▄', '▄', '▀' } },
 
-        { 'T', new[] { '▄', '▄', '▄', ' ', '█', ' ', ' ', '█', ' ' } },
+        { 'T', new[] { '▀', '█', '▀', ' ', '█', ' ', ' ', '█', ' ' } },
         { 't', new[] { ' ', '▄', ' ', '▀', '█', '▀', ' ', '▀', '▄' } },
 
         { 'U', new[] { '▄', ' ', '▄', '█', ' ', '█', '█', '▄', '█' } },
-        { 'u', new[] { '▄', ' ', '▄', '█', '▄', '█', ' ', ' ', ' ' } },
+        { 'u', new[] { ' ', ' ', ' ', '▄', ' ', '▄', '▀', '▄', '█' } },
 
         { 'V', new[] { '▄', ' ', '▄', '█', ' ', '█', '▀', '▄', '▀' } },
-        { 'v', new[] { '▄', ' ', '▄', '▀', '▄', '▀', ' ', ' ', ' ' } },
+        { 'v', new[] { ' ', ' ', ' ', '▄', ' ', '▄', '▀', '▄', '▀' } },
 
         { 'W', new[] { '█', ' ', '█', '█', ' ', '█', '█', '▀', '█' } },
-        { 'w', new[] { '█', ' ', '█', '█', '▀', '█', ' ', ' ', ' ' } },
+        { 'w', new[] { ' ', ' ', ' ', '█', ' ', '█', '█', '▀', '█' } },
 
-        { 'X', new[] { '█', ' ', '█', '█', ' ', ' ', '█', ' ', '█' } },
-        { 'x', new[] { '▄', ' ', '▄', '▀', '▄', '▀', '█', ' ', '█' } },
+        { 'X', new[] { '█', ' ', '█', '▄', '▀', '▄', '█', ' ', '█' } },
+        { 'x', new[] { ' ', ' ', ' ', '▀', '▄', '▀', '█', ' ', '█' } },
 
-        { 'Y', new[] { '█', ' ', '█', '▀', '█', '▀', '█', ' ', '█' } },
+        { 'Y', new[] { '█', ' ', '█', '▀', '█', '▀', ' ', '█', ' ' } },
         { 'y', new[] { '▄', ' ', '▄', '▀', '▄', '▀', '▄', '▀', ' ' } },
 
-        { 'Z', new[] { '▀', '▀', '█', '█', ' ', ' ', '█', '▄', '▄' } },
-        { 'z', new[] { '▄', '▄', '▄', '▄', '▀', ' ', '█', '▄', '▄' } }
+        { 'Z', new[] { '▀', '▀', '█', ' ', '█', ' ', '█', '▄', '▄' } },
+        { 'z', new[] { '▄', '▄', '▄', ' ', '▄', '▀', '█', '▄', '▄' } }
     };
 
-    public BigTextTextMask(Component component, string text, byte horizontalPadding = 0, byte verticalPadding = 0,
+
+    private static readonly Dictionary<FontType, (int size, Dictionary<char, char[]>)> _getFont = new()
+    {
+        { FontType.ThreeByThreeClassic, (3, ThreeByThreeClassic) },
+    };
+
+    public FontType Font
+    {
+        get;
+        set
+        {
+            field = value;
+            NeedReDraw = true;
+        }
+    }
+
+    public BigTextMask(Component component, string text, FontType fontType = FontType.ThreeByThreeClassic,
+        byte horizontalPadding = 0, byte verticalPadding = 0,
         HorizontalAlignmentEnum horizontalAlignment = HorizontalAlignmentEnum.Left,
         VerticalAlignmentEnum verticalAlignment = VerticalAlignmentEnum.Top, bool isVisible = true,
         Terminal.TextColor color = Terminal.TextColor.White,
@@ -91,6 +114,7 @@ public class BigTextTextMask : TextMask
         Terminal.TextDecoration decoration = Terminal.TextDecoration.Default) : base(component, text, horizontalPadding,
         verticalPadding, horizontalAlignment, verticalAlignment, isVisible, color, background, decoration)
     {
+        Font = fontType;
     }
 
     protected override void Behaviour()
@@ -105,22 +129,23 @@ public class BigTextTextMask : TextMask
 
     private (int newY, int newX) DrawLetter(int y, int x, char letter)
     {
-        if ((x + 3) > Component.Width)
+        (int size, Dictionary<char, char[]> font) = _getFont[Font];
+        if ((x + size) > Component.Width)
         {
-            y += 4;
+            y += size + 1;
             x = 0;
         }
 
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < size; i++)
         {
-            for (int j = 0; j < 3; j++)
+            for (int j = 0; j < size; j++)
             {
-                char c = _font.ContainsKey(letter) ? _font[letter][i * 3 + j] : ' ';
+                char c = ThreeByThreeClassic.ContainsKey(letter) ? font[letter][i * size + j] : ' ';
 
                 DrawChar(y + i, x + j, c);
             }
         }
 
-        return (y, x + 5);
+        return (y, x + (int)(size * 1.8));
     }
 }
--- Program.cs
+++ Program.cs
@@ -17,59 +17,15 @@ ImageMask bgImageMask = new ImageMask(bg, "Images/wallpaper.jpg");
 */
 
 Component component = new Component(terminal, -1, -1, 1, 1);
-BigTextTextMask bigTextTextMask =
-    new BigTextTextMask(component,
-        "Aa Bb Cc Dd Ee Ff Gg Hh Ii Jj Kk Ll Mm Nn Oo Pp Qq Rr Ss Tt Uu Vv Ww Xx Yy Zz Ceci est une phrase de test pour voir comment les lettres s'arranges entre elles");
-
-/*
-Component window = new Component(terminal, 15, 10, -1, -1);
-BoxMask windowBoxMask = new BoxMask(window, BoxMask.Type.Double, true, Terminal.TextColor.Blue);
+BigTextMask bigTextMask = new BigTextMask(component,
+    "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG the quick brown fox jumps over the lazy dog");
 
 terminal.InputSystem.OnKeyPress += key =>
 {
-    switch (key.Key)
+    if (key.Key == ConsoleKey.Tab)
     {
-        case ConsoleKey.LeftArrow:
-        {
-            if (key.Modifiers == ConsoleModifiers.Shift)
-                window.Width--;
-            else
-                window.PosX--;
-            break;
-        }
-        case ConsoleKey.RightArrow:
-        {
-            if (key.Modifiers == ConsoleModifiers.Shift)
-                window.Width++;
-            else
-                window.PosX++;
-            break;
-        }
-        case ConsoleKey.UpArrow:
-        {
-            if (key.Modifiers == ConsoleModifiers.Shift)
-                window.Height--;
-            else
-                window.PosY--;
-            break;
-        }
-        case ConsoleKey.DownArrow:
-        {
-            if (key.Modifiers == ConsoleModifiers.Shift)
-                window.Height++;
-            else
-                window.PosY++;
-            break;
-        }
-        case ConsoleKey.Tab:
-        {
-            windowBoxMask.BoxType = (BoxMask.Type)((int)(windowBoxMask.BoxType + 1) % 7);
-            break;
-        }
+        bigTextMask.Font = (BigTextMask.FontType)(((int)bigTextMask.Font + 1) % 2);
     }
-
-    windowBoxMask.NeedReDraw = true;
 };
-*/
 
 terminal.Start();
